042857a8a2efd8e7d94f5dfa3df5568bf89ebc6f
elsehow <yes@cosmopol.is>
2015-10-27 14:18:04 -0700

    forgotten refactors

--- neurosky-browser/lib/mindwave-client.js
+++ neurosky-browser/lib/mindwave-client.js
@@ -1,22 +1,23 @@
 var Mindwave = require('mindwave2')
   , JSONStream = require('JSONStream')
+  , Kefir = require('kefir')
   // debug
-  , raw512 = require('lodash').flatten(require('./raw512.js'))
-  , raws = require('kefir').sequentially(2, raw512)
+  //, raw512 = require('lodash').flatten(require('./raw512.js'))
+  //, raws = require('kefir').sequentially(2, raw512)
   // EEG processing stuff
   , fftjs = require('fft-js')
   , fft = fftjs.fft
   , freq = function (d) { return fftjs.util.fftFreq(d,512) }
   , magnitudes = function (raws) { return fftjs.util.fftMag(fft(raws)) }
 
-var stringifier = JSONStream.stringifyObject()
+var stringifier = JSONStream.stringify()
 function writeFFT (d) {
-  stringifier.write(['fft', d])
+  stringifier.write(d)
 }
 
 // process raw mindwave data
-//var mw = new Mindwave();
-//Kefir.fromEvents(mw, 'wave')
+var mw = new Mindwave();
+var raws = Kefir.fromEvents(mw, 'wave')
 raws
      .slidingWindow(512,512)
      .throttle(10)
@@ -24,6 +25,6 @@ raws
      .onValue(writeFFT)
 
 ////connect to mw
-//mw.connect('/dev/cu.MindWaveMobile-DevA')
+mw.connect('/dev/cu.MindWaveMobile-DevA')
 
 stringifier.pipe(process.stdout)
--- neurosky-browser/server.js
+++ neurosky-browser/server.js
@@ -3,13 +3,20 @@ var http = require('http')
   , spawn = require('child_process').spawn
   , es = require('event-stream')
   , mindwaveClientPath = __dirname + '/lib/mindwave-client.js'
+  , debugMindwaveClientPath = __dirname + '/lib/fake-mindwave-client.js'
   , ecstatic = require('ecstatic')(__dirname + '/public')
   , server = http.createServer(ecstatic)
+  , argv = process.argv.slice(2)
+  , debug = argv[0] === 'debug'
+
+console.log('debug?', debug)
 
 server.listen(8000)
 
-// spawn node mindwaveCleintPath
-var c = spawn('node', [mindwaveClientPath])
+if (debug) 
+  var c = spawn('node', [debugMindwaveClientPath])
+else 
+  var c = spawn('node', [mindwaveClientPath])
 
 var sock = shoe(function (stream) {
   c.stdout.pipe(stream)
